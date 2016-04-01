b00cee4ee18cef57ae562157956ce52ca0dea467
Charalampos Karypidis <xaralaboskaripidis@gmail.com>
2016-04-01 23:07:08 +0200

    Create the default MVC project and update all nuget packages

---
+++ GlobalAzureBootcampReport/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GlobalAzureBootcampReport.Startup))]
+namespace GlobalAzureBootcampReport
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
