42c1188083cc176323c272cb435e65a35dc99a7d
Charalampos Karypidis <xaralaboskaripidis@gmail.com>
2016-04-09 14:52:13 +0200

    Add development webpack configuration file and remote not needed images

---
+++ GlobalAzureBootcampReport/webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'wwwroot'),
+    filename: 'bundle.js',
+    publicPath: 'http://localhost:4000/'
+  },
+  resolve: {
+    root: path.resolve(__dirname),
+    extensions: ['', '.js', '.jsx'],
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css/,
+        loader: 'style!css!postcss',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
+        loader: "url?limit=10000"
+      }
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    host: "0.0.0.0",
+    port: "4000",
+    colors: true,
+    historyApiFallback: true,
+    hot: true,
+    inline: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
